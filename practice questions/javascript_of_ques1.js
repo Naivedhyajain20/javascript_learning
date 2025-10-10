e4e350dfc5f34aefebc83366fedd21b90a5a1314
Naivedhyajain20 <naivedhyajain1234@gmail.com>
2025-10-11 01:41:32 +0530

    ques1

---
+++ practice questions/javascript_of_ques1.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector("button");
+var p = document.querySelector("p");
+
+btn.addEventListener("click", function(){
+    p.textContent = "haa pata hai tu chutiya hai...ab to sudher ja bkl kahi bhi dabata raheta hai jaha button dikh jaye......";
+})
