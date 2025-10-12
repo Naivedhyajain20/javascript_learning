26aadd87cc143dfe50cf1707c26f5f41b5b5d0df
Naivedhyajain20 <naivedhyajain1234@gmail.com>
2025-10-12 14:14:33 +0530

    ques3

---
+++ practice questions/javascript_of_ques3.js
@@ -0,0 +1,17 @@
+var form = document.querySelector("form")
+var inp1 = document.querySelector("#inp1")
+var inp2 = document.querySelector("inp2")
+var h4 = document.querySelector("h4")
+
+form.addEventListener("submit" , function(ev){
+    ev.preventDefault();
+if(inp1.value === '' || inp2.value === ''){
+        h4.textContent = "ERROR...Kuch to daal";
+    h4.style.color = "red";
+}
+else{
+        h4.textContent = '' ;
+        h4.style.color = "black" ;
+}
+
+})
